f87da5ca32531d95c4ff8b8971669586e7945c10
Денис Вергун <blueagle@gnail.com>
2025-02-27 15:19:07 +0000

    "CPPFLAGS="-g "  make mainZ;./mainZ"

---
+++ mainZ.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using FI = void(int);
+
+void Foo(int x){
+std::cout<<x<<std::endl;
+};
+class Pro{
+public:
+FI &job=Foo;
+
+
+};
+
+class Чат{
+//FI &fun;
+public:
+Чат(Pro p){rf=p.job;};
+FI *rf=NULL;
+
+};
+//rf(42); // тоже самое, что и Foo(42);
+int main(){
+Pro p;
+Чат *ч= new Чат(p);
+ч->rf(42);
+
+}

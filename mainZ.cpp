664236c64f5d940a4fd27e45042b73dbfee901bb
Денис Вергун <blueagle@gnail.com>
2025-02-28 05:08:25 +0000

    "CPPFLAGS="-g "  make mainZ;./mainZ"

--- mainZ.cpp
+++ mainZ.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using FI = void(int);
 
 void Foo(int x){
@@ -14,14 +15,23 @@ FI &job=Foo;
 class Чат{
 //FI &fun;
 public:
-Чат(Pro p){rf=p.job;};
+Чат(Pro p):rf(p.job){};
 FI *rf=NULL;
 
 };
+
+class Team{
+public:
+int x;
+
+};
+
 //rf(42); // тоже самое, что и Foo(42);
 int main(){
 Pro p;
 Чат *ч= new Чат(p);
 ч->rf(42);
+std::vector<Team> t;
+t.push_back({3});
 
 }
